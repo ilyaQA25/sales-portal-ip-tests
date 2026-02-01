554e2eecf63fe9cbc2a98d2a68ccf7ce93f982f8
ilya2.0 <iprez4031@gmail.com>
2026-02-01 14:24:05 +0300

    LoginTestUI; smoke test and one negative test LoginTestAPI; adding variables

---
+++ src/test/java/tests/LoginTestAPI.java
@@ -0,0 +1,6 @@
+package tests;
+
+public class LoginTestAPI {
+    private static final String BASE_URL = "http://localhost:8686"; // Обратите внимание на порт 8686
+    private static final String LOGIN_ENDPOINT = "/api/auth/login";
+}
