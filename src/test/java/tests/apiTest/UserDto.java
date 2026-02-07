ccfcb769001f9c78fd5a2af56ac9259b095d22e7
ilya2.0 <iprez4031@gmail.com>
2026-02-07 12:42:10 +0300

    LoginTestAPI; Creating positive and negative loginApi tests

---
+++ src/test/java/tests/apiTest/UserDto.java
@@ -0,0 +1,20 @@
+package tests.apiTest;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@JsonIgnoreProperties(ignoreUnknown = true)
+public class UserDto {
+    private String _id;
+    private String username;
+    private String firstName;
+    private String lastName;
+    private List<String> roles;
+}
